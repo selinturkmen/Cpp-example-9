c128c8f2c4c76ec3045704cd209dc0899bf85433
selinturkmen <86002571+selinturkmen@users.noreply.github.com>
2022-02-17 15:06:48 +0300

    Add files via upload

---
+++ pe9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include "Complex.h"
+int main(){
+	Complex a(1, 7), b(9, 2), c;
+	a.printComplex();
+	cout << "+";
+	b.printComplex();
+	cout << "=";
+	c = a.add(b);
+	c.printComplex();
+	cout << "\n";
+	a.setComplexNumber(10, 1);
+	b.setComplexNumber(11, 5);
+	a.printComplex();
+	cout << "-";
+	b.printComplex();
+	cout << "=";
+	c = a.subtract(b);
+	c.printComplex();
+	cout << endl;
+return 0;}
